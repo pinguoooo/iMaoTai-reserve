372d7ec186719807cb18f9fe97584037be5c1f94
Hkirito <572745813@qq.com>
2024-04-29 11:55:08 +0800

    myself information2

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ aes_key = privateCrypt.get_aes_key()
 s_title = '茅台预约成功'
 s_content = ""
 
+
 for section in configs.sections():
     if (configs.get(section, 'enddate') != 9) and (TODAY > configs.get(section, 'enddate')):
         continue
